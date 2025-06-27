c55eaf69f209f9e4cd8e55a0b8d3e2aed0cc2d1d
Cem Aksoylar <caksoylar@users.noreply.github.com>
2025-06-27 16:35:33 -0700

    support multiple

--- rss.py
+++ rss.py
@@ -1,4 +1,6 @@
 import sys
+import time
+from datetime import datetime, timezone
 
 import feedparser
 from mako.template import Template
@@ -9,14 +11,19 @@ def main():
 
     with open(sys.argv[1]) as f:
         feeds = f.read().splitlines()
-    print(feeds)
+
+    entries = []
     for feed in feeds:
         d = feedparser.parse(feed)
-        print(d["feed"]["title"])
+        title = d["feed"]["title"]
+        for entry in d["entries"]:
+            entry.published_datetime = datetime.fromtimestamp(time.mktime(entry["published_parsed"]), tz=timezone.utc)
+            entry.source_title = title
+            entries.append(entry)
 
+    entries.sort(key=lambda x: x.published_datetime, reverse=True)
     with open(sys.argv[2], "w") as fo:
-        fo.write(template.render(feed=feedparser.parse(feeds[1])))
-
+        fo.write(template.render(all_entries=entries))
 
 
 if __name__ == "__main__":
